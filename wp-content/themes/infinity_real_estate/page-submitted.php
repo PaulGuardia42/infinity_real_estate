7a5831fe7972b1dbd11bfa159480908d8595fc8a
Paul Guardia <81693555+PaulGuardia42@users.noreply.github.com>
2022-03-03 14:54:44 -0400

    changes made on Thu, Mar  3, 2022  2:54:44 PM

--- wp-content/themes/infinity_real_estate/page-submitted.php
+++ wp-content/themes/infinity_real_estate/page-submitted.php
@@ -1,1 +1,13 @@
-!!!
+<?php get_header('front'); ?>
+
+<div class="success-container">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/transparent_infinity_logo_1.png" alt="MLS Logo">
+    <?php 
+        while(have_posts()): the_post(); ?>
+        <h1><?php the_title();?></h1>
+       <?php  the_content();
+    endwhile;
+    ?>
+</div>
+
+<?php get_footer(); ?>
