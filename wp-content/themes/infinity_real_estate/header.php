2432c241a202a3739def75f53176c7df9a21358b
Paul Guardia <81693555+PaulGuardia42@users.noreply.github.com>
2022-03-04 15:51:32 -0400

    changes made on Fri, Mar  4, 2022  3:51:32 PM

--- wp-content/themes/infinity_real_estate/header.php
+++ wp-content/themes/infinity_real_estate/header.php
@@ -4,19 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/preview-img.png" />
-
+    
     
     <title>Paul Guardia Design</title>
 
     <link rel="icon" type="image/png" href="" sizes="32x32"/>
     <?php wp_head() ?>
-
+    
     <!-- SEO -->
-    <meta name="description" content="PGD"/>
-    <meta name="keywords" content="Design, Programming, Development"/>
-    <meta name="author" content="Paul Guardia" />
-
+    <meta name="description" content="Infinity Real Estate"/>
+    <meta name="keywords" content="Real Estate, Halifax, Nova Scotia"/>
+    <meta name="author" content="Jason Paul" />
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/preview-img.png" />
+    
 
 </head>
 <body>
