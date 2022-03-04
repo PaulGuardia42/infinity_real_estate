aabe7f48a44ff9c6752036761a28bb63374b4211
Paul Guardia <81693555+PaulGuardia42@users.noreply.github.com>
2022-03-04 15:47:03 -0400

    changes made on Fri, Mar  4, 2022  3:47:02 PM

--- wp-content/themes/infinity_real_estate/header.php
+++ wp-content/themes/infinity_real_estate/header.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/preview-img.png" />
+
     
     <title>Paul Guardia Design</title>
 
