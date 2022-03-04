b21f2f07b25e46b2c2d4d4f9b7ff04ea21980faf
Paul Guardia <81693555+PaulGuardia42@users.noreply.github.com>
2022-03-04 15:28:59 -0400

    changes made on Fri, Mar  4, 2022  3:28:59 PM

--- wp-content/themes/infinity_real_estate/footer.php
+++ wp-content/themes/infinity_real_estate/footer.php
@@ -9,12 +9,7 @@
         location = '<?php echo get_template_directory_uri() . "/submitted" ?>'
     }, false);
     
-    if ( document.URL.includes("<?php echo get_site_url() . "/sign-up-form/" ?>") ) {
-        const badge = document.querySelectorAll("div");
-            console.log(badge);
-        
-        //   badge.classList.add('showgr');
-    }
+
 </script>
     <?php wp_footer(); ?>
 </body>
