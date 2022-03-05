08f5b1834f622a29cfb6d5c15caa487a021dee15
Paul Guardia <81693555+PaulGuardia42@users.noreply.github.com>
2022-03-05 17:37:35 -0400

    changes made on Sat, Mar  5, 2022  5:37:35 PM

--- wp-content/themes/infinity_real_estate/functions.php
+++ wp-content/themes/infinity_real_estate/functions.php
@@ -13,10 +13,10 @@
 
 
 //Remove Gutenberg Block Library CSS from loading on the frontend
-function smartwp_remove_wp_block_library_css(){
-    wp_dequeue_style( 'wp-block-library' );
-    wp_dequeue_style( 'wp-block-library-theme' );
-    wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
-} 
-add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+// function smartwp_remove_wp_block_library_css(){
+//     wp_dequeue_style( 'wp-block-library' );
+//     wp_dequeue_style( 'wp-block-library-theme' );
+//     wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+// } 
+// add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
 ?>
