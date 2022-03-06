dfad104454758fb611b74ba7e373b862681a531f
Paul Guardia <81693555+PaulGuardia42@users.noreply.github.com>
2022-03-06 10:16:46 -0400

    changes made on Sun, Mar  6, 2022 10:16:46 AM

--- wp-content/themes/infinity_real_estate/header.php
+++ wp-content/themes/infinity_real_estate/header.php
@@ -15,10 +15,16 @@
     <meta name="description" content="Infinity Real Estate"/>
     <meta name="keywords" content="Real Estate, Halifax, Nova Scotia"/>
     <meta name="author" content="Jason Paul" />
+
+    <!-- Social Media Preview -->
+    <meta property="og:title" content="Infinity Real Estate" />
     <meta property="og:description" content="Infinity Real Estate" />
+    <meta property="og:url" content="https://paulg147.sg-host.com/" />
     <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/preview-img.png" />
     <meta property="og:image:secure_url" content="<?php echo get_template_directory_uri(); ?>/images/preview-img.png" /> 
     <meta property="og:image:type" content="image/png" /> 
+
+
 
 
 
